3aa641fc8fdf3ee6bcea0e283df91a0779000767
Rahul Bobade <43333447+rahulbobade@users.noreply.github.com>
2019-10-24 09:32:01 +0530

    updated clear screen

--- clear_screen.py
+++ clear_screen.py
@@ -1,6 +1,15 @@
+'''
 import os 
 import platform
 if platform.system()=="windows":
    os.system("cls")
 else:
    os.sys("clear")
+'''
+
+import os 
+import platform
+if platform.system()=="windows":
+    print(os.system("cls"))
+else:
+    print(os.system("clear"))
