07c6017cba8b64bf4655781a8628adb9341f7053
Rahul Bobade <43333447+rahulbobade@users.noreply.github.com>
2019-12-21 19:14:27 +0530

    Update execute_cmd.py

--- execute_cmd.py
+++ execute_cmd.py
@@ -8,3 +8,9 @@ print(f'The return code: {rc}')
 print(f'The output : \n{out}')
 print(f'The error : \n{err}')
 
+'''
+if shell=True then your cmd is a string (as your os command)
+if shell=False then your cmd is a list 
+
+shell=False dont work on os environment variable.
+'''
