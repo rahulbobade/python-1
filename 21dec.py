fc7c1c1a303a8ee83e1ca18d5ae83551f32fff42
Rahul Bobade <43333447+rahulbobade@users.noreply.github.com>
2019-12-21 18:51:23 +0530

    subprocess module

---
+++ 21dec.py
@@ -0,0 +1,10 @@
+import subprocess
+cmd="ls -lrth"
+sp=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+rc=sp.wait()
+out,err=sp.communicate()
+
+print(f'The return code: {rc}')
+print(f'The output : \n{out}')
+print(f'The error : \n{err}')
+
